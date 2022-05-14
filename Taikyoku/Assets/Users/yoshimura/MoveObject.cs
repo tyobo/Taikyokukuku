90ebb3f4ef45f1a467e8de7773be25db5ac8454f
201-17\student <eere>
2022-05-14 18:17:49 +0900

    進捗

--- Taikyoku/Assets/Users/yoshimura/MoveObject.cs
+++ Taikyoku/Assets/Users/yoshimura/MoveObject.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveObject : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,15 @@ public class MoveObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //-x座標に移動し続ける
+        transform.position += new Vector3(-1, 0, 0) * Time.deltaTime*3;
+    }
+
+    //コライダー2dを持つオブジェクトと衝突したらログが出る
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log("Hit"); // ログを表示する
+
     }
 }
